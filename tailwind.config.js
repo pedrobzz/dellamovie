4b54de2984b396b19d91b9440f68e4174689c775
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-15 22:58:27 -0300

    Added new dependencies Co-authored-by: Pedro Bzz <pedrobzz@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/pages/**/*.tsx", "./src/common/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
