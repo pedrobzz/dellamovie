73f8181c677382fc1248fd7683cf245ce9bfa1fb
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-15 23:10:14 -0300

    feat(header): added header component Co-authored-by: Pedro Bzz <57912928+pedrobzz@users.noreply.github.com>

---
+++ src/common/components/Header/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const Header: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, Header!</h1>
+    </div>
+  );
+};
+
+export default Header;
