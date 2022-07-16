3a3875c1a717e0c8e4ad8718f76667223c4759f3
pedro bzz <pedrobzz66@gmail.com>
2022-07-16 01:27:13 -0300

    remove(domain): Removed current Domain because we are going to use tRPC. Most of these are uselles NOW. Needs to be added an updated version to the database tho
    Co-authored-by: Francielle Dellamora <francielle.dellamora@venturus.org.br>

--- src/domain/use-cases/movies/getMovieDetails.ts
+++
@@ -1,8 +0,0 @@
-import { Movie } from "../../entities"
-export interface GetMovieDetails {
- getMovieDetails(id: string): Promise<Movie>
-}
-
-export namespace GetMovieDetails {
-  export const endpoint = '/movie/';
-}
