7d825a3986910b584b6674ff8bc1f860659dc80a
pedro bzz <pedrobzz66@gmail.com>
2022-07-16 02:30:20 -0300

    update jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   injectGlobals: true,
   transform: {
-    "^.+\\.spec.ts": ["@swc/jest"],
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
   },
 };
--- src/application/factories/makeTheMovieDB.ts
+++ src/application/factories/makeTheMovieDB.ts
@@ -2,6 +2,8 @@ import MovieDB from "node-themoviedb";
 let mdb: MovieDB;
 export const makeTheMovieDB = (): MovieDB => {
   if (!mdb) {
+    if (!process.env.TMDB_API_KEY)
+      throw new Error("TMDB_API_KEY is not defined");
     mdb = new MovieDB(process.env.TMDB_API_KEY, { language: "pt-BR" });
   }
   return mdb;
