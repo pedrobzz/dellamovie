9b17e43462a618a1da4e2f34c21bc0ad8cb35bf7
pedro bzz <pedrobzz66@gmail.com>
2022-07-16 01:42:41 -0300

    fix theMovieDB Language

--- src/application/factories/makeTheMovieDB.ts
+++ src/application/factories/makeTheMovieDB.ts
@@ -2,7 +2,7 @@ import MovieDB from "node-themoviedb";
 let mdb: MovieDB;
 export const makeTheMovieDB = (): MovieDB => {
   if (!mdb) {
-    mdb = new MovieDB(process.env.TMDB_API_KEY);
+    mdb = new MovieDB(process.env.TMDB_API_KEY, { language: "pt-BR" });
   }
   return mdb;
 };
