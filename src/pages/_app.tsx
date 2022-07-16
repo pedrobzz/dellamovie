540deef8baf5a4dd755e219f1631528136df973a
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-15 23:13:04 -0300

    feat(pages): implemented tailwind, authcontext and base layout Co-authored-by: Pedro Bzz <57912928+pedrobzz@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from "next/app";
-import "../common/styles/login.css"
-
+import "../common/styles/login.css";
+import "../common/styles/globals.css";
 
 import { AppContextProvider } from "../common/context/appContext";
+import { AuthContextProvider } from "../common/context/authContext";
+import Header from "../common/components/Header";
+import Navigation from "../common/components/Navigation";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <AppContextProvider>
-        <Component {...pageProps} />
+        <AuthContextProvider>
+          <Header />
+          <Component {...pageProps} />
+          <Navigation />
+        </AuthContextProvider>
       </AppContextProvider>
     </>
   );
