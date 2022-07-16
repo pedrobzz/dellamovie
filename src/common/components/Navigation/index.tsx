05db2da54a9de481c12cf52ff5cc5e7eaca77d16
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-15 23:10:36 -0300

    feat(navigation): added navigation component Co-authored-by: Pedro Bzz <57912928+pedrobzz@users.noreply.github.com>

---
+++ src/common/components/Navigation/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const Navigation: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, Navigation!</h1>
+    </div>
+  );
+};
+
+export default Navigation;
