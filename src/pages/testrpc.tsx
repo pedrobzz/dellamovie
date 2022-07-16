8a39128a77c8f1090ebf4638e9bc948521b3278d
pedro bzz <pedrobzz66@gmail.com>
2022-07-16 02:10:46 -0300

    feat(testrpc): added a base route to test recommendations
    Co-authored-by: Francielle Dellamora <francielle.dellamora@venturus.org.br>

--- src/pages/testrpc.tsx
+++ src/pages/testrpc.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import Image from "next/image";
 import React from "react";
 import { useTRPC } from "../common/hooks/useTRPC";
 
@@ -12,7 +13,21 @@ const TestRPC: React.FC = (): JSX.Element => {
     <div className="flex flex-col w-full items-center">
       <h1>This is a page to Test TRPC.</h1>
       <h1>Movies</h1>
-      {/* {moviesQuery.data && <></>} */}
+      <div className="grid grid-cols-10 gap-10">
+        {moviesQuery.data &&
+          moviesQuery.data.results.map(movie => {
+            return (
+              <div key={movie.id}>
+                <Image
+                  src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`}
+                  width={300}
+                  height={450}
+                ></Image>
+                {movie.title}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
