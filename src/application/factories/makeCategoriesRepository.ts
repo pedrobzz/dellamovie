97c57b120eb1cc209a07da1ccefe439ebc4c904f
pedro bzz <pedrobzz66@gmail.com>
2022-07-16 05:55:04 -0300

    update when get categoriesRepository

--- src/application/factories/makeCategoriesRepository.ts
+++ src/application/factories/makeCategoriesRepository.ts
@@ -3,6 +3,15 @@ import { PrismaCategoriesRepository } from "../infra/repositories";
 import { makePrismaClient } from "./makePrismaClient";
 import { makeTheMovieDB } from "./makeTheMovieDB";
 
+let categoriesRepository: CategoriesRepositoryModel;
 export const makeCategoriesRepository = (): CategoriesRepositoryModel => {
-  return new PrismaCategoriesRepository(makePrismaClient(), makeTheMovieDB());
+  if (!categoriesRepository) {
+    categoriesRepository = new PrismaCategoriesRepository(
+      makePrismaClient(),
+      makeTheMovieDB(),
+    );
+    categoriesRepository.updateAllCategories();
+  }
+
+  return categoriesRepository;
 };
