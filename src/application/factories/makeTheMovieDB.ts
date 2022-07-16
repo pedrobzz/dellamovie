63dab57f54a1e95e4396da165e38bcb84cfce0af
pedro bzz <pedrobzz66@gmail.com>
2022-07-15 23:47:56 -0300

    feat(TheMovideDB): Added TheMovieDB factory

---
+++ src/application/factories/makeTheMovieDB.ts
@@ -0,0 +1,8 @@
+import MovieDB from "node-themoviedb";
+let mdb: MovieDB;
+export const makeTheMovieDB = (): MovieDB => {
+  if (!mdb) {
+    mdb = new MovieDB(process.env.TMDB_API_KEY);
+  }
+  return mdb;
+};
