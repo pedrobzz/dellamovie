17ec0ccdf2227caf3ce1d8a14ed1be2d0d6d4898
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-15 23:08:44 -0300

    feat(trpc): added  trpc adn hooks Co-authored-by: Pedro Bzz <57912928+pedrobzz@users.noreply.github.com>

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,13 @@
+import * as trpc from "@trpc/server";
+import * as trpcNext from "@trpc/server/adapters/next";
+// import { pokemonController } from "../../../application/controller/";
+
+export const appRouter = trpc.router();
+
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
