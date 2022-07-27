c6011f73b84af7efe35bac42da1012d93f16e750
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-27 20:38:26 -0300

    [WIP]

--- src/common/components/Navigation/index.tsx
+++ src/common/components/Navigation/index.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from "react";
-
+import Logo from "../../svgs/logo.svg";
+import SearchIcon from "../../svgs/searchIcon.svg";
+import StarIcon from "../../svgs/starIcon.svg";
+import ChatIcon from "../../svgs/chatIcon.svg";
 /* import { Container } from "./styles"; */
 
 const Navigation: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1> Hello, Navigation!</h1>
+    <div className="flex items-center  justify-between p-5 h-2">
+      <Logo />
+      <SearchIcon />
+      <StarIcon />
+      <ChatIcon />
     </div>
   );
 };
