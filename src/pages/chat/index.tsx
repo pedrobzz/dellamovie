8d6f25c5efe8666c5991b9961379f30131391531
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-27 20:35:43 -0300

    added new pages

---
+++ src/pages/chat/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const chat: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, chat!</h1>
+    </div>
+  );
+};
+
+export default chat;
---
+++ src/pages/explore/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+/* import { Container } from "./styles"; */
+
+const explore: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1> Hello, explore!</h1>
+    </div>
+  );
+};
+
+export default explore;
