8bc7dca5e7c08bcca8f7404e6e9dd7f76afa7263
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-27 21:41:39 -0300

    [WIP]

--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from "react";
-
+import LogoTitleIcon from "../../svgs/LogoTitle";
 /* import { Container } from "./styles"; */
 
 const Header: React.FC = (): JSX.Element => {
-  return (
-    <div>
-      <h1> Hello, Header!</h1>
-    </div>
-  );
+  return <LogoTitleIcon onClick={console.log} />;
 };
 
 export default Header;
