7cf5e98e69550d2877aa1a358e3bcd784c9f8e9e
Francielle Dellamora <francielle.dellamora@venturus.org.br>
2022-07-29 19:22:00 -0300

    [WIP] card's style

---
+++ src/common/components/cards/index.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import MovieDB from "node-themoviedb";
+import React from "react";
+/* import { Container } from "./styles"; */
+
+interface CardsProps {
+  data: MovieDB.Responses.Trending.GetTrending;
+  onClick: () => void;
+}
+
+const cards: React.FC<CardsProps> = ({ data, onClick }): JSX.Element => {
+  return (
+    <div className="relative content-center z-1">
+      {data &&
+        data.results.map(movie => (
+          <div key={movie.id} className="overflow-hidden rounded-lg absolute ">
+            <img
+              className="cover"
+              src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`}
+            />
+          </div>
+        ))}
+      <button onClick={onClick} />
+    </div>
+  );
+};
+
+export default cards;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
-import { Title } from "../common/components/Title";
+import Cards from "../common/components/cards";
 import AppContext from "../common/context/appContext";
+import { useTRPC } from "../common/hooks/useTRPC";
 
 const Home = (): JSX.Element => {
+  const trpc = useTRPC();
   const ctx = useContext(AppContext);
+  const { data, isLoading } = trpc.useQuery(["recomendations.getTrending"]);
+
   return (
-    <div className="h-full bg-[#acabab]">
-      <Title>My page</Title>
-      <h3 className="text-3xl text-red-500">Context Name: </h3>
+    <div className="h-[90vh]  bg-[#acabab] relative overflow-hidden flex-col">
+      <Cards data={data} onClick={console.log} />
     </div>
   );
 };
